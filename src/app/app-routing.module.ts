1a8bff436d2a610235e78b2be3ba561eff9b0560
Frank.He <frank.he@datavisor.com>
2022-01-03 13:57:52 -0800

    demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from "@app/app.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: AppComponent,
+  children:[
+    {
+      path: '',
+      redirectTo:'demo/page1',
+      pathMatch: 'full'
+    }
+  ]
+}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
